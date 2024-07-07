83e05de06826b9b05ce57693b378551aa1a3cf7b
thinh <anvanthinh@gmail.com>
2024-07-07 21:30:12 +0700

    create base class with user model

--- src/test/java/com/thinh/timemanagementservice/TimeManagementServiceApplicationTests.java
+++ src/test/java/com/thinh/timemanagementservice/TimeManagementServiceApplicationTests.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TimeManagementServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test() {
         Instant expectedDate = Instant.parse("2020-09-08T12:16:40Z");
         long seconds = 1599567400L;
 
