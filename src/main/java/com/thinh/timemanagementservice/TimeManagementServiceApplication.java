60014491cc900ec24cbf8b9a1ebeb03ef2f277cc
thinh <anvanthinh@gmail.com>
2024-07-07 22:51:48 +0700

    update config

--- src/main/java/com/thinh/timemanagementservice/TimeManagementServiceApplication.java
+++ src/main/java/com/thinh/timemanagementservice/TimeManagementServiceApplication.java
@@ -1,11 +1,11 @@
 package com.thinh.timemanagementservice;
 
-import com.thinh.timemanagementservice.mapper.WorkDayMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TimeManagementServiceApplication {
 
 	public static void main(String[] args) {
